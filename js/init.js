18758bbdc21f94d90ff89ba5cb082b66b033f86b
Daniel Juncos <daniel.juncos.01@gmail.com>
2017-05-26 19:55:28 -0400

    parallax

--- js/init.js
+++ js/init.js
@@ -2,7 +2,31 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+    $('.parallax1').parallax();
+
+    $('.parallax2').parallax();
+
+    $(window).scroll(function() {
+		  var scrollTop = $(this).scrollTop();
+
+		  $('.header-overlay').css({
+			  opacity: function() {
+			    var elementHeight = $(this).height(),
+			    	opacity = scrollTop/600;
+			        // opacity = scrollTop/(elementHeight - pageHeight);
+
+			        // console.log("pageHeight = "+pageHeight);
+			        // console.log("elementHeight = "+elementHeight);
+			        // console.log("opacity = "+opacity);
+			        // console.log("scrollTop = "+scrollTop);
+
+			    return opacity;
+			  }
+			});
+		});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var pageHeight = $(window.top).height();
+
