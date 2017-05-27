2ea5703d477a0fc64d80fb74989884f786712d2e
Daniel Juncos <daniel.juncos.01@gmail.com>
2017-05-26 20:55:54 -0400

    parallax again

--- js/init.js
+++ js/init.js
@@ -22,8 +22,38 @@
 
 			    return opacity;
 			  }
-			});
-		});
+		   });
+
+		   $('.header-overlay2').css({
+			  opacity: function() {
+			    var elementHeight = $(this).height(),
+			    	opacity = scrollTop/600 - 2.5;
+			        // opacity = scrollTop/(elementHeight - pageHeight);
+
+			        // console.log("pageHeight = "+pageHeight);
+			        // console.log("elementHeight = "+elementHeight);
+			        // console.log("opacity = "+opacity);
+			        // console.log("scrollTop = "+scrollTop);
+
+			    return opacity;
+			  }
+		   });
+
+		   $('.header-overlay3').css({
+			  opacity: function() {
+			    var elementHeight = $(this).height(),
+			    	opacity = scrollTop/600 - 4.5;
+			        // opacity = scrollTop/(elementHeight - pageHeight);
+
+			        // console.log("pageHeight = "+pageHeight);
+			        // console.log("elementHeight = "+elementHeight);
+			        // console.log("opacity = "+opacity);
+			        // console.log("scrollTop = "+scrollTop);
+
+			    return opacity;
+			  }
+		   });
+	});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
