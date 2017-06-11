d9fb7e5167a2e1412183d997fc84c1711c1190a8
Daniel Juncos <daniel.juncos.01@gmail.com>
2017-06-11 05:08:13 -0400

    fade on hover

--- js/init.js
+++ js/init.js
@@ -6,33 +6,38 @@
 
     $('.parallax2').parallax();
 
-    $(window).scroll(function() {
-		  var scrollTop = $(this).scrollTop();
+ //    $(window).scroll(function() {
+	// 	  var scrollTop = $(this).scrollTop();
 
-		  $('.header-overlay').css({
-			  opacity: function() {
-			    var elementHeight = $(this).height(),
-			    	opacity = scrollTop/600;
-			    return opacity;
-			  }
-		   });
+	// 	  $('.header-overlay').css({
+	// 		  opacity: function() {
+	// 		    var elementHeight = $(this).height(),
+	// 		    	opacity = scrollTop/600;
+	// 		    return opacity;
+	// 		  }
+	// 	   });
 
-		   $('.header-overlay2').css({
-			  opacity: function() {
-			    var elementHeight = $(this).height(),
-			    	opacity = scrollTop/600 - 2.5;
-			    return opacity;
-			  }
-		   });
+	// 	   $('.header-overlay2').css({
+	// 		  opacity: function() {
+	// 		    var elementHeight = $(this).height(),
+	// 		    	opacity = scrollTop/600 - 2.5;
+	// 		    return opacity;
+	// 		  }
+	// 	   });
 
-		   $('.header-overlay3').css({
-			  opacity: function() {
-			    var elementHeight = $(this).height(),
-			    	opacity = scrollTop/600 - 4.5;
-			    return opacity;
-			  }
-		   });
-	});
+	// 	   $('.header-overlay3').css({
+	// 		  opacity: function() {
+	// 		    var elementHeight = $(this).height(),
+	// 		    	opacity = scrollTop/600 - 4.5;
+	// 		    return opacity;
+	// 		  }
+	// 	   });
+	// });
+
+	$('.trigger').hover(
+       function(){ $('#roseAfter').addClass('appears') },
+       function(){ $('#roseAfter').removeClass('appears') }
+)
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
